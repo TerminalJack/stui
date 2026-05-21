24f8b929d216ffa0c9d134981c139b98114f2336
TerminalJack <TerminalJack@sbcglobal.net>
2026-05-21 15:34:45 -0500

    The SpatialAdapter component has been optimized now that its ancestors are guaranteed to run before it is via the DependencyResolver component..

--- Assets/Stui/Runtime/SpatialAdapter.cs
+++ Assets/Stui/Runtime/SpatialAdapter.cs
@@ -17,11 +17,15 @@ namespace Stui
         public Vector2 Position = Vector2.zero;
         public Vector2 Scale = Vector2.one;
 
-        private readonly List<SpatialAdapter> _cachedSpatialAdapters = new List<SpatialAdapter>();
+        [NonSerialized] public Vector2 runningScale = Vector2.one; // The scale of this component * its ancestors' scales.
+
+        private SpatialAdapter _parentSpatialAdapter = null; // If non-null then found on direct parent.
+        private SpatialAdapter _virtualParentSpatialAdapter = null; // If non-null then found thru virtual parent.
+
         private readonly List<ScaleTracker> _cachedScaleTrackers = new List<ScaleTracker>();
 
         private readonly List<VirtualParent> _cachedVirtualParents = new List<VirtualParent>();
-        private readonly List<int> _cachedVersions = new List<int>();
+        private readonly List<int> _cachedVpVersions = new List<int>();
 
         private bool _isSpritePivotOrCollider; // If false then the component is on a bone or action point.
         private SpatialController _spatialController;
@@ -42,16 +46,16 @@ namespace Stui
             // The animation curves will use the Spatial Controller component to control whether to use Spriter
             // scaling or not.  When importing, it should be created before any SpatialAdapter components.
             _spatialController = GetComponentInParent<SpatialController>();
-        }
 
-        void OnEnable()
-        {
             if (_spatialController == null)
             {   // This is a programming error and shouldn't happen in production.  (Or the user deleted it.)
                 Debug.LogWarning("A SpatialController component could not be found.");
             }
+        }
 
-            ResolveChain();
+        void OnEnable()
+        {
+            ResolveAncestorChain();
         }
 
         public void ApplyTransformModifier()
@@ -68,21 +72,11 @@ namespace Stui
                 return;
             }
 
-            ResolveChainIfNeeded();
+            Vector2 ancestorScale = GetAncestorScale();
 
-            Vector2 finalLocalScale = Vector2.one;
+            runningScale = Scale * ancestorScale; // Save this for any children.
 
-            foreach (var spatialAdapter in _cachedSpatialAdapters)
-            {
-                finalLocalScale *= spatialAdapter.Scale;
-            }
-
-            foreach (var scaleTracker in _cachedScaleTrackers)
-            {
-                finalLocalScale *= scaleTracker.RawScale;
-            }
-
-            finalLocalScale = new Vector2(Mathf.Abs(finalLocalScale.x), Mathf.Abs(finalLocalScale.y));
+            Vector2 finalLocalScale = new Vector2(Mathf.Abs(ancestorScale.x), Mathf.Abs(ancestorScale.y));
 
             transform.localPosition = Position * finalLocalScale;
 
@@ -97,8 +91,42 @@ namespace Stui
                     1f);
         }
 
-        private void ResolveChainIfNeeded()
+        private Vector2 GetAncestorScale()
         {
+            ResolveAncestorChainIfNeeded();
+
+            if (_parentSpatialAdapter != null)
+            {
+                return _parentSpatialAdapter.runningScale;
+            }
+            else if (_virtualParentSpatialAdapter != null)
+            {
+                return _virtualParentSpatialAdapter.runningScale;
+            }
+            else if (_cachedScaleTrackers.Count > 0)
+            {
+                Vector2 resultScale = Vector2.one;
+
+                foreach (var st in _cachedScaleTrackers)
+                {
+                    resultScale *= st.RawScale;
+                }
+
+                return resultScale;
+            }
+            else
+            {
+                return Vector2.one;
+            }
+        }
+
+        private void ResolveAncestorChainIfNeeded() // Don't call this without calling ResolveAncestorChain() at least once.
+        {
+            if (_parentSpatialAdapter != null)
+            {
+                return;
+            }
+
             // If there are no VirtualParents in the chain then the chain will always be valid and will need to be
             // resolved only once.
             if (_cachedVirtualParents.Count == 0)
@@ -112,43 +140,44 @@ namespace Stui
             {
                 var vp = _cachedVirtualParents[i];
 
-                if (vp != null && vp.Version != _cachedVersions[i])
+                if (vp != null && vp.Version != _cachedVpVersions[i])
                 {
-                    ResolveChain();
+                    ResolveAncestorChain();
 
                     return;
                 }
             }
         }
 
-        private void ResolveChain()
+        private void ResolveAncestorChain()
         {
-            _cachedSpatialAdapters.Clear();
+            _parentSpatialAdapter = null;
+            _virtualParentSpatialAdapter = null;
+
             _cachedScaleTrackers.Clear();
             _cachedVirtualParents.Clear();
-            _cachedVersions.Clear();
+            _cachedVpVersions.Clear();
 
             Transform t = transform.parent;
 
+            if (t.TryGetComponent(out _parentSpatialAdapter))
+            {
+                return;
+            }
+
             int depth = 0; // Used to guard against cycles.
 
-            while (t != null && ++depth < 100)
+            while (t != null && depth < 100)
             {
-                if (t.TryGetComponent(out SpatialAdapter s))
-                {
-                    _cachedSpatialAdapters.Add(s);
-                }
-
                 if (t.TryGetComponent(out ScaleTracker tracker))
                 {
                     _cachedScaleTrackers.Add(tracker);
+                    t = t.parent;
                 }
-
-                // Cache VirtualParent and its version, if present.
-                if (t.TryGetComponent(out VirtualParent vp))
+                else if (t.TryGetComponent(out VirtualParent vp)) // Cache VirtualParent and its version, if present.
                 {
                     _cachedVirtualParents.Add(vp);
-                    _cachedVersions.Add(vp.Version);
+                    _cachedVpVersions.Add(vp.Version);
 
                     // Follow virtual parent redirection.  Note that this component will run during an import and, in
                     // that case, the virtual parent's possibleParents list can be empty for a short time.  The
@@ -156,10 +185,21 @@ namespace Stui
 
                     t = vp.GetVirtualParentTransform();
                 }
+                else if (t.TryGetComponent(out _virtualParentSpatialAdapter))
+                {
+                    if (depth != 1)
+                    {
+                        Debug.LogWarning("SpatialAdapter.GetAncestorScale(): An invalid ancestor chain was encountered.");
+                    }
+
+                    return;
+                }
                 else
                 {
-                    t = t.parent;
+                    return;
                 }
+
+                ++depth;
             }
         }
     }
